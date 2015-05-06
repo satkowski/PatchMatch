df91995d9dc30a73f40b5181e10bddb4f63e601a
Marc Satkowski <marc.satkowski@yahoo.de>
2015-05-06 19:42:29 +0200

    Bilder hinzugefügt.

--- CV2_Excersice1/Main.cpp
+++ CV2_Excersice1/Main.cpp
@@ -11,4 +11,12 @@ int main (int argc, const char** argv)
 	//Reading the Callingarguments
 	CommandLineParser parser (argc, argv, keyMap);
 	String filename = parser.get<String> ("path");
+
+	//Creating the image and testing if it is empty or not
+	Mat image = imread(filename);
+	if (image.empty ())
+	{
+		printf ("Cannot read the image %s\n", filename.c_str ());
+		return -1;
+	}
 }
--- CV2_Excersice1/Main.h
+++ CV2_Excersice1/Main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <opencv2/core/core.hpp>
+#include "opencv2/highgui/highgui.hpp"
 
 #include <stdio.h>
 
