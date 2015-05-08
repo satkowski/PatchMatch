b8f9d304a5ac02d79848cb2b249c33e4d41738bb
Marc Satkowski <marc.satkowski@yahoo.de>
2015-05-08 11:36:53 +0200

    Fehler behoben. - Es fehlte eine compare-Funktion für zwei Points welche dem set übergeben werden musste

--- CV2_Excersice1/PatchMatch.h
+++ CV2_Excersice1/PatchMatch.h
@@ -8,6 +8,11 @@
 
 #define ITERAATIONTIMES 5
 
+struct comparePoints
+{
+	bool operator()(const cv::Point &left, const cv::Point &right);
+};
+
 cv::Vec<cv::Mat, 2> calculateOpticalFlow(const cv::Mat* firstImage, const cv::Mat* secondImage);
 
 cv::Mat createInitialization(const cv::Mat* firstImage);
