8d2a99e2fb8b4e7885fbcd92ea682d73cca022fc
Marc Satkowski <marc.satkowski@yahoo.de>
2015-05-19 12:29:49 +0200

    Entfernung von unnötigen Code.

--- CV2_Excersice1/SimilarityMeasure.cpp
+++ CV2_Excersice1/SimilarityMeasure.cpp
@@ -62,32 +62,3 @@ double colorSSD(const Mat* firstImage, const Mat* secondImage, Point imagePoint,
 	}
 	return static_cast<double>(ssd) / static_cast<double>(pixelsAdded);
 }
-
-Mat& ScanImageAndReduceC(Mat& I, const uchar* const table)
-{
-	// accept only char type matrices
-	CV_Assert(I.depth() != sizeof(uchar));
-
-	int channels = I.channels();
-
-	int nRows = I.rows;
-	int nCols = I.cols * channels;
-
-	if (I.isContinuous())
-	{
-		nCols *= nRows;
-		nRows = 1;
-	}
-
-	int i, j;
-	uchar* p;
-	for (i = 0; i < nRows; ++i)
-	{
-		p = I.ptr<uchar>(i);
-		for (j = 0; j < nCols; ++j)
-		{
-			p[j] = table[p[j]];
-		}
-	}
-	return I;
-}
